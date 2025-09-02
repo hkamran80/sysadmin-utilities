94afbda47af395ee3505c424e7aef6d36a89e318
H. Kamran <hkamran@hkamran.com>
2025-09-01 19:28:34 -0700

    Add additional notes, write with `tee`

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 echo "This script will attempt to set up all utilities."
-echo "Additional setup may be required for webhooks, directories, and more."
+echo "If you wish to only set up some, edit the 'utilities' array in 'setup.sh'."
+echo "Additional setup may be required for webhooks, directories, and more. Sudo access is required to configure systemd."
+echo
+read -p "Press enter to continue."
 
 sudo -v
+
+echo
 
 utilities=(
     "startup-notify"
@@ -14,7 +19,7 @@ utilities=(
 for utility in "${utilities[@]}"; do
     echo "Setting up $utility..."
 
-    sed "s:/home/\$USER/sysadmin-utilities:$(pwd):" "$utility/$utility.service" > "/etc/systemd/system/$utility.service"
+    sed "s:/home/\$USER/sysadmin-utilities:$(pwd):" "$utility/$utility.service" | sudo tee "/etc/systemd/system/$utility.service" > /dev/null
     chmod +x "$utility/$utility.sh"
 
     sudo systemctl enable "$utility.service"
